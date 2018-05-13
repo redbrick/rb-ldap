a467788b2edb8bfec3cad0817f53f3d46af2444a
butlerx <butlerx@notthe.cloud>
2018-05-13 22:30:10 +0100

    add newYear command to rbuser

--- pkg/rbuser/rbuser.go
+++ pkg/rbuser/rbuser.go
@@ -11,7 +11,7 @@ import (
 type RbUser struct {
 	UID              string
 	UserType         string
-	ObjectClass      string
+	ObjectClass      []string
 	Newbie           bool   // New this year
 	CN               string // Full name
 	Altmail          string // Alternate email
@@ -31,41 +31,41 @@ type RbUser struct {
 	HomeDirectory    string
 	UserPassword     string   // Crypted password.
 	Host             []string // List of hosts.
-	ShadowLastChange time.Time
+	ShadowLastChange int
 }
 
 // Vhost reutrn apache  macro template
 func (u *RbUser) Vhost() string {
-	return fmt.Sprintf("use VHost /storage/webtree/%s/%s %s %s %s", string([]rune(u.UID)[0]), u.UID, u.UID, u.ObjectClass, u.UID)
+	return fmt.Sprintf("use VHost /storage/webtree/%s/%s %s %s %s", string([]rune(u.UID)[0]), u.UID, u.UID, u.UserType, u.UID)
 }
 
 // PrettyPrint output user info to command line
 func (u *RbUser) PrettyPrint() error {
-	const output = `
-	User Information
-	================
-	{{ with .UID }}uid: {{ . }}
-	{{ end }}{{ with .UserType }}usertype: {{ . }}
-	{{ end }}{{ with .ObjectClass }}objectClass: {{ . }}
-	{{ end }}{{ with .Newbie }}newbie: {{ . }}
-	{{ end }}{{ with .CN }}cn: {{ . }}
-	{{ end }}{{ with .Altmail }}altmail: {{ . }}
-	{{ end }}{{ with .ID }}id: {{ . }}
-	{{ end }}{{ with .Course }}course: {{ . }}
-	{{ end }}{{ with .Year }}year: {{ . }}
-	{{ end }}{{ with .YearsPaid }}yearsPaid: {{ . }}
-	{{ end }}{{ with .UpdatedBy }}updatedBy: {{ . }}
-	{{ end }}{{ with .Updated }}updated: {{ . }}
-	{{ end }}{{ with .CreatedBy }}createdby: {{ . }}
-	{{ end }}{{ with .Created }}created: {{ . }}
-	{{ end }}{{ with .UIDNumber }} uidNumber: {{ . }}
-	{{ end }}{{ with .GIDNumber }}gidNumber: {{ . }}
-	{{ end }}{{ with .Gecos }}gecos: {{ . }}
-	{{ end }}{{ with .LoginShell }}loginShell: {{ . }}{{ end }}
-	{{ end }}{{ with .HomeDirectory }}homeDirectory: {{ . }}{{ end }}
-	{{ end }}{{ with .UserPassword }}userPassword: {SHA}{{ . }}{{ end }}
-	{{ end }}{{ with .Host }}host: {{ . }}
-	{{ end }}{{ with .ShadowLastChange }}shadowLastChange: {{ . }}{{ end }}
+	const output = `User Information
+================
+{{ with .UID }}uid: {{ . }}
+{{ end }}{{ with .UserType }}usertype: {{ . }}
+{{ end }}{{ with .ObjectClass }}objectClass: {{ . }}
+{{ end }}{{ with .Newbie }}newbie: {{ . }}
+{{ end }}{{ with .CN }}cn: {{ . }}
+{{ end }}{{ with .Altmail }}altmail: {{ . }}
+{{ end }}{{ with .ID }}id: {{ . }}
+{{ end }}{{ with .Course }}course: {{ . }}
+{{ end }}{{ with .Year }}year: {{ . }}
+{{ end }}{{ with .YearsPaid }}yearsPaid: {{ . }}
+{{ end }}{{ with .UpdatedBy }}updatedBy: {{ . }}
+{{ end }}{{ with .Updated.Format "2006-01-02 15:04:05" }}updated: {{ . }}
+{{ end }}{{ with .CreatedBy }}createdby: {{ . }}
+{{ end }}{{ with .Created.Format "2006-01-02 15:04:05" }}created: {{ . }}
+{{ end }}{{ with .Birthday.Format "2006-01-02 15:04:05" }}birthday: {{ . }}
+{{ end }}{{ with .UIDNumber }}uidNumber: {{ . }}
+{{ end }}{{ with .GidNumber }}gidNumber: {{ . }}
+{{ end }}{{ with .Gecos }}gecos: {{ . }}
+{{ end }}{{ with .LoginShell }}loginShell: {{ . }}
+{{ end }}{{ with .HomeDirectory }}homeDirectory: {{ . }}
+{{ end }}{{ with .UserPassword }}userPassword: {{ . }}
+{{ end }}{{ with .Host }}host: {{ . }}
+{{ end }}{{ with .ShadowLastChange }}shadowLastChange: {{ . }}{{ end }}
 `
 
 	t := template.Must(template.New("user").Parse(output))
--- pkg/rbuser/update.go
+++ pkg/rbuser/update.go
@@ -15,10 +15,9 @@ func (rb *RbLdap) Update(user RbUser) error {
 	modification.Replace("altmail", []string{user.Altmail})
 	modification.Replace("course", []string{user.Course})
 	modification.Replace("year", []string{string(user.Year)})
-	modification.Replace("updated", []string{now.Format("2006-01-02 15:04:00")})
+	modification.Replace("updated", []string{now.Format(timeLayout)})
 	modification.Replace("updatedBy", []string{user.UpdatedBy})
 	modification.Replace("loginShell", []string{user.LoginShell})
-	modification.Replace("shadowlastchanged", []string{now.Format("2006-01-02 15:04:00")})
-	modification.Replace("birthday", []string{user.Birthday.Format("2006-01-02 15:04:00")})
+	modification.Replace("birthday", []string{user.Birthday.Format(timeLayout)})
 	return rb.Conn.Modify(modification)
 }
