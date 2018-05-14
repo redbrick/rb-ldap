b77e5b0e448b6bb6ff50cf415a67805b482dd173
butlerx <butlerx@notthe.cloud>
2018-05-14 19:28:51 +0100

    add commands for unpaid accounts

--- cmd/rb-ldap/alert-unpaid.go
+++ cmd/rb-ldap/alert-unpaid.go
@@ -11,3 +11,17 @@ var alertUnpaid = cli.Command{
 	Name:     "alert-unpaid",
 	Usage:    "Alert all unpaid users that their accounts will be disabled",
 }
+
+var deleteUnpaid = cli.Command{
+	Action:   rbldap.DeleteUnPaid,
+	Category: "Batch Commands",
+	Name:     "delete-unpaid",
+	Usage:    "Delete all unpaid users accounts that are outside their grace period",
+}
+
+var disableUnpaid = cli.Command{
+	Action:   rbldap.DisableUnPaid,
+	Category: "Batch Commands",
+	Name:     "disable-unpaid",
+	Usage:    "Diable all unpaid users accounts",
+}
